d229958b996f7309a5d68490696cc9c7126b0d1a
VinZ <97350963+VinZ007@users.noreply.github.com>
2025-12-21 16:36:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ timeButtons[2].classList.add('active');
 
 const PlantMusicDictionary = {
     narcissus: document.getElementById('narcissusmusic'),
-    seastones: document.getElementById('yellowmusic')
+    seastones: document.getElementById('yellowmusic'),
+    running: document.getElementById('runningman')
 };
 
 function updateTimerDisplay() {
@@ -103,7 +104,7 @@ function startTimer() {
             clearInterval(timerInterval);
             isRunning = false;
             startBtn.disabled = false;
-            // Останавливаем музыку при завершении таймера
+            
             if (currentMusic) {
                 currentMusic.pause();
                 currentMusic.currentTime = 0;
@@ -139,7 +140,6 @@ function stopTimer() {
     isRunning = false;
     startBtn.disabled = false;
     
-    // Останавливаем музыку при остановке таймера
     if (currentMusic) {
         currentMusic.pause();
         currentMusic.currentTime = 0;
@@ -211,7 +211,14 @@ function updatePlantImages(plantType) {
             'img/plant2_2.png',
             'img/plant2_3.png',
             'img/plant2_4.png'
+        ],
+        running: [
+            'img/plant3_1.png',
+            'img/plant3_2.png',
+            'img/plant3_3.png',
+            'img/plant3_4.png'
         ]
+
     };
 
     if (plantImages[plantType]) {
